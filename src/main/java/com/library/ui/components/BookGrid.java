fd2220c92f2baa3fb36d2093cffbde6b292e655f
Adam Rodrigues <arodrigues@stgeorges.bc.ca>
2026-02-26 13:15:52 -0800

    Added navigation/routing to book details page

---
+++ src/main/java/com/library/ui/components/BookGrid.java
@@ -0,0 +1,31 @@
+package com.library.ui.components;
+
+import com.library.backend.Book;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.data.provider.ListDataProvider;
+import utils.StringUtils;
+
+import java.util.List;
+
+public class BookGrid  extends Grid<Book> {
+    private final ListDataProvider<Book> dataProvider;
+
+    public BookGrid(List<Book> allBooks) {
+        dataProvider = new ListDataProvider<>(allBooks);
+        setDataProvider(dataProvider);
+        addColumn(Book::getTitle).setHeader("Title").setSortable(true);
+        addColumn(Book::getAuthor).setHeader("Author").setSortable(true);
+        addColumn(Book::getIsbn).setHeader("ISBN");
+    }
+
+    public void filter(String searchTerm) {
+        dataProvider.setFilter(book ->
+            searchTerm == null ||
+            searchTerm.isEmpty() ||
+            StringUtils.containsIgnoreCase(book.getTitle(), searchTerm) ||
+            StringUtils.containsIgnoreCase(book.getAuthor(), searchTerm) ||
+            StringUtils.containsIgnoreCase(book.getIsbn(), searchTerm)
+        );
+    }
+
+}
--- src/main/java/com/library/ui/views/BookDetails.java
+++ src/main/java/com/library/ui/views/BookDetails.java
@@ -4,28 +4,33 @@ import com.library.backend.Book;
 import com.library.backend.MockBookRepository;
 import com.library.ui.components.ViewToolbar;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.InMemoryDataProvider;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
 
+import java.util.Optional;
+
 @Route("books")
 public class BookDetails extends VerticalLayout implements HasUrlParameter<Long> {
     private final MockBookRepository bookRepo;
-    private Long bookId;
 
     public BookDetails(MockBookRepository bookRepo) {
         this.bookRepo = bookRepo;
     }
 
     @Override
-    public void setParameter(BeforeEvent beforeEvent, Long aLong) {
-        bookId = aLong;
-        Book book = bookRepo.findById(bookId).orElseThrow(); // TODO make this renavigate
-        add(new ViewToolbar(book.getTitle()));
-        createDetailsTable(book);
+    public void setParameter(BeforeEvent beforeEvent, Long bookId) {
+        bookRepo.findById(bookId).ifPresentOrElse(
+            this::showBookDetails,
+            () -> beforeEvent.forwardTo("books")
+        );
     }
 
-    private void createDetailsTable(Book book) {}
+    private void showBookDetails(Book book) {
+        add(new ViewToolbar(book.getTitle()));
+    }
 }
